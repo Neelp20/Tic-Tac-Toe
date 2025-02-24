4877974a135815fcac6363db0cb7929e09ec370d
Neelp20 <8f5sdjz8@students.codeinstitute.net>
2025-02-24 10:55:56 +0000

    Add Homepage link to game page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,79 @@
-// JavaScript code to ensure the Bootstrap mobile navbar collapses when navigating to in-page links
-    
-// document
-// .querySelectorAll(".navbar-collapse .nav-link")
-// .forEach((link) => {
-//     link.addEventListener("click", function (e) {
-//         let section = document.querySelector(e.target.getAttribute("href"));
-//         if (section) {
-//             e.preventDefault(); // Prevent default anchor click behavior
-//             let navbarHeight = document.querySelector(".navbar-toggler").offsetHeight;
-//             window.scroll({
-//                 top: section.offsetTop - navbarHeight, // Adjust for navbar height
-//                 behavior: "smooth",
-//             });
-//             document
-//                 .querySelector(".navbar-collapse")
-//                 .classList.remove("show"); // Collapse navbar
+
+// let boxes = document.querySelectorAll(".box");
+// let restartBtn = document.querySelector("#restart-btn");
+// let newGameBtn = document.querySelector("#new-btn");
+// let msgContainer = document.querySelector(".msg-container");
+// let msg = document.querySelector("#msg");
+
+// let turnO = true; //playerX, playerO
+
+// const winPatterns = [
+//     [0, 1, 2],
+//     [0, 3, 6],
+//     [0, 4, 8],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     [2, 4, 6],
+//     [3, 4, 5],
+//     [6, 7, 8],
+// ];
+
+// const restartBtn = () => {
+//     turnO = true;
+//     enableBoxes();
+//     msgContainer.classList.add("hide");
+// }
+
+// boxes.forEach((box) =>{
+//     box.addEventListener("click", () => {
+//         console.log("box was clicked");
+//         if (turnO) { 
+//             box.innerText = "O";
+//             turnO = false;    
+//         } else { 
+//             box.innerText = "X";
+//             turnO = true;
 //         }
+//         box.disabled = true; 
+
+//         checkWinner();
 //     });
 // });
-    
+
+// const disableBoxes = () => {
+//     for (let box of boxes) {
+//         box.disabled = true;
+//     }
+// }
+
+// const enableBoxes = () => {
+//     for (let box of boxes) {
+//         box.disabled = false;
+//         box.innerText = "";
+//     }
+// }
+
+// const showWinner = (winner) => {
+//     msg.innerText = `Congratulations, Winner is ${winner}`;
+//     msgContainer.classList.remove("hide");
+//     disableBoxes();
+// };
+
+// const checkWinner = () => {
+//     for (let pattern of winPatterns) {
+//         let pos1Val = boxes[pattern[0]].innerText;
+//         let pos2Val = boxes[pattern[1]].innerText;
+//         let pos3Val = boxes[pattern[2]].innerText;
+
+//         if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+//             if (pos1Val === pos2Val && pos2Val === pos3Val) {
+//                 console.log("winner", pos1Val);
+//                 showWinner(pos1Val);
+//             }
+//         }
+//     }
+// };
+
+// newGameBtn.addEventListener("click", resetGame);
+// restartBtn.addEventListener("click", resetGame)
 
